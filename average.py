a3157c934233ab2a4c81d0b792560e302f3f1d59
utishtr-jpg <utishtr@gmail.com>
2025-12-10 20:22:11 -0800

    Add files via upload

---
+++ average.py
@@ -0,0 +1,11 @@
+print("This will calculate the sum and average of the trees in Jack's yard.")
+tree_1=98
+tree_2=94
+tree_3=96
+tree_4=11
+tree_5=41
+sum= (tree_1 + tree_2 + tree_3 + tree_4 + tree_5)
+average = (tree_1 + tree_2 + tree_3 + tree_4 + tree_5) / 5
+print("The sum of the trees is:", sum)
+print("The average of the trees is:", average)
+print("End of program.")
