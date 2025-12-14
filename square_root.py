eff6b8f700fbeade33674ebdc709f1ea53e662d4
utishtr-jpg <utishtr@gmail.com>
2025-12-13 21:32:39 -0800

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,9 @@
+
+import math
+
+
+print("Welcome to the square root calculator!")
+print("You ready? /nLet's begin!")
+num = float(input("Please enter a number: "))
+print("The square root of your number is:", math.sqrt(num))
+print("Please have a nice day!")
